ed1e69f79affd59947fe4bbb2ee0cc7a5241db47
marcus-elia <mselia@uvm.edu>
2019-11-26 14:28:56 -0500

    Added Wall, an abstract class to be extended by StraightWall and CurvedWall

---
+++ src/Wall.java
@@ -0,0 +1,17 @@
+public abstract class Wall
+{
+    // The points are swept out clockwise from the 0 angle
+    protected Point p1;
+    protected Point p2;
+    protected Point p3;
+    protected Point p4;
+
+    protected Point center;
+
+    public Wall()
+    {
+
+    }
+
+    public abstract double distanceFromWall(Point p);
+}
