4bca05ca5005eccc4bf9f4a13f6ee3a33ebc2963
marcus-elia <mselia@uvm.edu>
2019-11-26 16:34:34 -0500

    RinkBorder now also has the curved walls

--- src/RinkBorder.java
+++ src/RinkBorder.java
@@ -15,6 +15,10 @@ public class RinkBorder
     private StraightWall topWall;
     private StraightWall rightWall;
     private StraightWall bottomWall;
+    private CurvedWall topLeftWall;
+    private CurvedWall topRightWall;
+    private CurvedWall bottomRightWall;
+    private CurvedWall bottomLeftWall;
 
     public RinkBorder(GameManager inputManager, int inputWidth, int inputHeight, double inputThickness)
     {
@@ -34,6 +38,7 @@ public class RinkBorder
     // ==========================================
     public void initializeWalls()
     {
+        // The straight walls on the 4 sides
         this.leftWall = new StraightWall(new Point(this.thickness/2, this.height/2),
                 this.thickness, 3*this.height/4);
         this.rightWall = new StraightWall(new Point(this.width - this.thickness/2, this.height/2),
@@ -43,11 +48,25 @@ public class RinkBorder
         this.bottomWall = new StraightWall(new Point(this.width/2, this.height - this.thickness/2),
                 this.width - this.height/4, this.thickness);
 
+        // The curved walls in the 4 corners
+        this.topLeftWall = new CurvedWall(new Point(this.height/8, this.height/8),
+                this.height/8, this.thickness, Math.PI, 3*Math.PI/2);
+        this.topRightWall = new CurvedWall(new Point(this.width - this.height/8, this.height/8),
+                this.height/8, this.thickness, 3*Math.PI/2, 2*Math.PI);
+        this.bottomRightWall = new CurvedWall(new Point(this.width - this.height/8, 7*this.height/8),
+                this.height/8, this.thickness, 0, Math.PI/2);
+        this.bottomLeftWall = new CurvedWall(new Point(this.height/8, 7*this.height/8),
+                this.height/8, this.thickness, Math.PI/2, Math.PI);
+
         this.walls = new ArrayList<Wall>();
         this.walls.add(leftWall);
         this.walls.add(rightWall);
         this.walls.add(topWall);
         this.walls.add(bottomWall);
+        this.walls.add(topLeftWall);
+        this.walls.add(topRightWall);
+        this.walls.add(bottomRightWall);
+        this.walls.add(bottomLeftWall);
     }
 
     public void tick()
