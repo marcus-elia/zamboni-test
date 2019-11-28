1bf9b82e43715d4e8f25a65b9b3c162b331a72e5
marcus-elia <mselia@uvm.edu>
2019-11-28 09:31:30 -0500

    Defined the updateSpeed function, with helper functions increaseSpeed and decreaseSpeed

---
+++ src/Zamboni.java
@@ -0,0 +1,127 @@
+import java.awt.*;
+import java.awt.geom.Path2D;
+
+public class Zamboni
+{
+    // Location and Size
+    private double x;
+    private double y;
+    private double xWidth;
+    private double yWidth;
+    private double angle;
+
+    // Hitbox
+    private Rectangle hitbox;
+
+    // Speed
+    private double curSpeed;
+    private double maxSpeed;
+    private double vx;
+    private double vy;
+    private double acceleration;
+    private double deltaTheta;
+
+    // User input
+    private boolean gasPedal;
+    private boolean brakePedal;
+    private boolean steeringRight;
+    private boolean steeringLeft;
+
+    private Color color;
+
+    private GameManager manager;
+    private RinkBorder border;
+
+    public Zamboni(GameManager manager, RinkBorder border, double inputX, double inputY, double inputXWidth,
+                   double inputYWidth, double inputAngle, double inputMaxSpeed,
+                   double inputAcceleration, double inputDeltaTheta, Color inputColor)
+    {
+        this.manager = manager;
+        this.border = border;
+        this.x = inputX;
+        this.y = inputY;
+        this.xWidth = inputXWidth;
+        this.yWidth = inputYWidth;
+        this.angle = inputAngle;
+        this.color = inputColor;
+
+        this.maxSpeed = inputMaxSpeed;
+        this.curSpeed = 0;
+        this.vx = 0;
+        this.vy = 0;
+        this.acceleration = inputAcceleration;
+        this.deltaTheta = inputDeltaTheta;
+
+        this.gasPedal = false;
+        this.brakePedal = false;
+        this.steeringRight = false;
+        this.steeringLeft = false;
+    }
+
+    public void tick()
+    {
+
+    }
+
+    public void render(Graphics2D g2d)
+    {
+        g2d.setColor(this.color);
+        Path2D path = new Path2D.Double();
+        path.moveTo(this.hitbox.getCorner1().x, this.hitbox.getCorner1().y);
+        path.lineTo(this.hitbox.getCorner2().x, this.hitbox.getCorner2().y);
+        path.lineTo(this.hitbox.getCorner3().x, this.hitbox.getCorner3().y);
+        path.lineTo(this.hitbox.getCorner4().x, this.hitbox.getCorner4().y);
+        path.lineTo(this.hitbox.getCorner1().x, this.hitbox.getCorner1().y);
+        path.closePath();
+        g2d.fill(path);
+    }
+
+    // ===========================================
+    //
+    //            Movement Functions
+    //
+    // ===========================================
+
+    // Update the velocity components to be consistent with the speed and angle
+    public void setVelocityComponents()
+    {
+        this.vx = this.curSpeed*Math.cos(this.angle);
+        this.vy = this.curSpeed*Math.sin(this.angle);
+    }
+
+    // Increase the speed by the amount input, or set it to maxSpeed, whichever is smaller
+    public void increaseSpeed(double amount)
+    {
+        this.curSpeed = Math.min(this.curSpeed + amount, this.maxSpeed);
+    }
+    // Decrease the speed by the amount input, or set it to minSpeed, whichever is bigger
+    public void decreaseSpeed(double amount)
+    {
+        this.curSpeed = Math.max(this.curSpeed - amount, 0);
+    }
+
+    // Update the magnitude of the speed based on the gas pedal and brake pedal input
+    public void updateSpeed()
+    {
+        if(this.gasPedal && this.brakePedal)
+        {
+            // Do not change the speed if both gas and brake are pressed
+            return;
+        }
+        // Holding the gas
+        else if(this.gasPedal)
+        {
+            this.increaseSpeed(this.acceleration);
+        }
+        // Braking
+        else if(this.brakePedal)
+        {
+            this.decreaseSpeed(this.acceleration / 3);
+        }
+        // Coasting
+        else
+        {
+            this.decreaseSpeed(this.acceleration / 6);
+        }
+    }
+}
