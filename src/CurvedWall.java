9ced159897c995d38ced7fa10627a5de191ea2a7
marcus-elia <mselia@uvm.edu>
2019-11-29 08:59:30 -0500

    Replaced anticenters with Bezier points to make the curve circle-like

--- src/CurvedWall.java
+++ src/CurvedWall.java
@@ -8,10 +8,11 @@ public class CurvedWall extends Wall
     private double startAngle;
     private double endAngle;
 
-    // On the opposite side of the curve from the center,
-    // used as the Bezier point
-    private Point outerAntiCenter;
-    private Point innerAntiCenter;
+    // Used for making the bezier curve approximate a circle
+    private Point innerBezier1;
+    private Point innerBezier2;
+    private Point outerBezier1;
+    private Point outerBezier2;
 
 
 
@@ -26,14 +27,19 @@ public class CurvedWall extends Wall
         this.outerRadius = inputOuterRadius;
         this.innerRadius = outerRadius - thickness;
 
-        // In order to get the anticenters correct, we need to know the sign of
+
+        // In order to get the Bezier points, we need to know the sign of
         // the sine and cosine of angles swept out by this curve.
         double middleAngle = (startAngle + endAngle) / 2;
         double signOfCos = Math.abs(Math.cos(middleAngle)) / Math.cos(middleAngle);
         double signOfSin = Math.abs(Math.sin(middleAngle)) / Math.sin(middleAngle);
+        double c = 0.551915024494; // from spencermortensen.com
 
-        this.outerAntiCenter = new Point(center.x + outerRadius*signOfCos, center.y + outerRadius*signOfSin);
-        this.innerAntiCenter = new Point(center.x + innerRadius*signOfCos, center.y + innerRadius*signOfSin);
+        this.innerBezier1 = new Point(center.x + innerRadius*c*signOfCos, center.y + innerRadius*signOfSin);
+        this.innerBezier2 = new Point(center.x + innerRadius*signOfCos, center.y + innerRadius*c*signOfSin);
+
+        this.outerBezier1 = new Point(center.x + outerRadius*c*signOfCos, center.y + outerRadius*signOfSin);
+        this.outerBezier2 = new Point(center.x + outerRadius*signOfCos, center.y + outerRadius*c*signOfSin);
 
         this.p1 = new Point(center.x + innerRadius*Math.cos(startAngle), center.y + innerRadius*Math.sin(startAngle));
         this.p2 = new Point(center.x + outerRadius*Math.cos(startAngle), center.y + outerRadius*Math.sin(startAngle));
@@ -53,9 +59,9 @@ public class CurvedWall extends Wall
         Path2D path = new Path2D.Double();
         path.moveTo(p1.x, p1.y);
         path.lineTo(p2.x, p2.y);
-        path.curveTo(outerAntiCenter.x, outerAntiCenter.y, outerAntiCenter.x, outerAntiCenter.y, p3.x, p3.y);
+        path.curveTo(outerBezier1.x, outerBezier1.y, outerBezier2.x, outerBezier2.y, p3.x, p3.y);
         path.lineTo(p4.x, p4.y);
-        path.curveTo(innerAntiCenter.x, innerAntiCenter.y, innerAntiCenter.x, innerAntiCenter.y, p1.x, p1.y);
+        path.curveTo(innerBezier2.x, innerBezier2.y, innerBezier1.x, innerBezier1.y, p1.x, p1.y);
         path.closePath();
         g2d.fill(path);
     }
