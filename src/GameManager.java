0c36e7ca7a2872cf1cbf43fb96968384a9e25a1f
marcus-elia <mselia@uvm.edu>
2019-12-03 21:10:30 -0500

    Slowed down everything about the zamboni

--- src/GameManager.java
+++ src/GameManager.java
@@ -16,7 +16,7 @@ public class GameManager
         this.border = new RinkBorder(this, this.windowWidth, this.windowHeight, 4);
 
         this.zamboni = new Zamboni(this, this.border, 4 + 20, windowHeight/2,
-                60, 40, 3*Math.PI/2, 3, .2, .05,
+                60, 40, 3*Math.PI/2, 1, .1, .025,
                 Math.PI/6, new Color(20, 40, 100));
 
         this.surface = new IceSurface(4, this.windowWidth, this.windowHeight);
