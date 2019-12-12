2c32afb2a46ece0cda649abeefac12dc1233a755
marcus-elia <mselia@uvm.edu>
2019-12-11 20:58:35 -0500

    Made the ZamboniButton class. It currently draws the snow tank and the body

---
+++ src/ZamboniButton.java
@@ -0,0 +1,74 @@
+import java.awt.*;
+import java.awt.geom.Path2D;
+
+// A button shaped like a zamboni. Click on it to start the game
+public class ZamboniButton
+{
+    private int x;
+    private int y;
+
+    private Path2D snowTank;
+    private Path2D body;
+
+    public ZamboniButton(int inputX, int inputY)
+    {
+        this.x = inputX;
+        this.y = inputY;
+
+        this.makeSnowTank();
+        this.makeBody();
+    }
+
+    // =====================================
+    //
+    //             Helper Functions
+    //           for the Constructor
+    //
+    // =====================================
+    public void makeSnowTank()
+    {
+        snowTank = new Path2D.Double();
+        snowTank.moveTo(x,y);
+        snowTank.lineTo(x - 120, y - 6);
+        snowTank.lineTo(x - 120, y - 30);
+        snowTank.lineTo(x, y - 48);
+        snowTank.lineTo(x,y);
+    }
+    private void makeBody()
+    {
+        body = new Path2D.Double();
+        body.moveTo(x,y);
+        body.lineTo(x - 120, y - 6);
+        body.lineTo(x - 120, y + 30);
+        body.lineTo(x - 114, y + 30);
+        body.lineTo(x - 96, y + 12);
+        body.lineTo(x - 78, y + 30);
+        body.lineTo(x - 18, y + 30);
+        body.lineTo(x, y + 12);
+        body.lineTo(x + 42, y + 12);
+        body.lineTo(x + 54, y);
+        body.lineTo(x + 54, y - 18);
+        body.lineTo(x + 42, y - 18);
+        body.lineTo(x + 42, y);
+        body.lineTo(x + 18, y);
+        body.lineTo(x + 18, y - 24);
+        body.lineTo(x + 24, y - 24);
+        body.lineTo(x + 24, y - 48);
+        body.lineTo(x, y - 48);
+        body.lineTo(x, y);
+    }
+
+
+
+
+
+
+    public void render(Graphics2D g2d)
+    {
+        g2d.setColor(Color.white);
+        g2d.fill(this.snowTank);
+
+        g2d.setColor(Color.blue);
+        g2d.fill(this.body);
+    }
+}
