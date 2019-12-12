9fa83e8ef37e484e44932a26ea8ffb66fb5f79e6
marcus-elia <mselia@uvm.edu>
2019-12-11 21:08:16 -0500

    The body now uses Bezier curves around the wheels

--- src/ZamboniButton.java
+++ src/ZamboniButton.java
@@ -41,16 +41,24 @@ public class ZamboniButton
         body.lineTo(x - 120, y - 6);
         body.lineTo(x - 120, y + 30);
         body.lineTo(x - 114, y + 30);
-        body.lineTo(x - 96, y + 12);
-        body.lineTo(x - 78, y + 30);
+
+        // Front wheel
+        body.curveTo(x - 114, y + 20.0655, x - 105.93347, y + 12, x - 96, y + 12);
+        body.curveTo(x - 86.0655, y + 12, x - 78, y + 20.0655, x - 78, y + 30);
         body.lineTo(x - 18, y + 30);
-        body.lineTo(x, y + 12);
+
+        // Back Wheel
+        body.curveTo(x - 18, y + 20.0655, x - 9.93447, y + 12, x, y + 12);
         body.lineTo(x + 42, y + 12);
+
+        // Seat
         body.lineTo(x + 54, y);
         body.lineTo(x + 54, y - 18);
         body.lineTo(x + 42, y - 18);
         body.lineTo(x + 42, y);
         body.lineTo(x + 18, y);
+
+        // Dashboard
         body.lineTo(x + 18, y - 24);
         body.lineTo(x + 24, y - 24);
         body.lineTo(x + 24, y - 48);
