f41945559a32861f68987bd77af051bd2ae00866
marcus-elia <mselia@uvm.edu>
2019-12-11 21:31:38 -0500

    Made a wheel class for the Zamboni Button to use

---
+++ src/Wheel.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+
+// A wheel to be drawn on a ZamboniButton
+public class Wheel
+{
+    private int x;
+    private int y;
+    private int outerRadius;
+    private int innerRadius;
+
+    private Ellipse2D tire;
+    private Ellipse2D innerWheel;
+
+    public Wheel(int inputX, int inputY, int inputOuterRadius, int inputInnerRadius)
+    {
+        this.x = inputX;
+        this.y = inputY;
+        this.outerRadius = inputOuterRadius;
+        this.innerRadius = inputInnerRadius;
+
+        this.tire = new Ellipse2D.Double(x - outerRadius, y - outerRadius, 2*outerRadius, 2*outerRadius);
+        this.innerWheel = new Ellipse2D.Double(x - innerRadius, y - innerRadius, 2*innerRadius, 2*innerRadius);
+    }
+
+    public void render(Graphics2D g2d)
+    {
+        g2d.setColor(Color.black);
+        g2d.fill(this.tire);
+
+        g2d.setColor(Color.white);
+        g2d.fill(this.innerWheel);
+    }
+}
