fda2942be397b28126093509e68e093e43315ba0
marcus-elia <mselia@uvm.edu>
2019-12-12 20:10:43 -0500

    The Wheel class now has colors that can change alpha

--- src/Wheel.java
+++ src/Wheel.java
@@ -10,8 +10,13 @@ public class Wheel
     private int outerRadius;
     private int innerRadius;
 
+    private boolean isHighlighted;
+
     private Ellipse2D tire;
+    private Color tireColor;
+
     private Ellipse2D innerWheel;
+    private Color innerWheelColor;
 
     public Wheel(int inputX, int inputY, int inputOuterRadius, int inputInnerRadius)
     {
@@ -20,16 +25,20 @@ public class Wheel
         this.outerRadius = inputOuterRadius;
         this.innerRadius = inputInnerRadius;
 
+        this.isHighlighted = false;
+
         this.tire = new Ellipse2D.Double(x - outerRadius, y - outerRadius, 2*outerRadius, 2*outerRadius);
+        this.tireColor = new Color(0,0,0,128);
         this.innerWheel = new Ellipse2D.Double(x - innerRadius, y - innerRadius, 2*innerRadius, 2*innerRadius);
+        this.innerWheelColor = new Color(255, 255, 200, 128);
     }
 
     public void render(Graphics2D g2d)
     {
-        g2d.setColor(Color.black);
+        g2d.setColor(this.tireColor);
         g2d.fill(this.tire);
 
-        g2d.setColor(Color.white);
+        g2d.setColor(this.innerWheelColor);
         g2d.fill(this.innerWheel);
     }
 
@@ -38,5 +47,24 @@ public class Wheel
     public boolean contains(Point2D p)
     {
         return this.tire.contains(p);
+    }
+
+    public void setIsHighlighted(boolean input)
+    {
+        if(input)
+        {
+            this.tireColor = new Color(this.tireColor.getRed(), this.tireColor.getGreen(),
+                    this.tireColor.getBlue(), 255);
+            this.innerWheelColor = new Color(this.innerWheelColor.getRed(), this.innerWheelColor.getGreen(),
+                    this.innerWheelColor.getBlue(), 255);
+        }
+        else
+        {
+            this.tireColor = new Color(this.tireColor.getRed(), this.tireColor.getGreen(),
+                    this.tireColor.getBlue(), 128);
+            this.innerWheelColor = new Color(this.innerWheelColor.getRed(), this.innerWheelColor.getGreen(),
+                    this.innerWheelColor.getBlue(), 128);
+        }
+        this.isHighlighted = input;
     }
 }
